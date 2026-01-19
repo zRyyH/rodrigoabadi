3cb4c121535490029b1fec741b7c7c7550ace6ad
zRyyH <zryyh.br@gmail.com>
2026-01-19 08:54:07 -0300

    v1.1

--- backend/express/src/app.js
+++ backend/express/src/app.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import routes from './routes/index.js';
 
 const app = express();
+
+app.use(cors({
+    origin: 'https://rodrigoabadi.com.br',
+    credentials: true
+}));
 
 app.use(express.json());
 app.use('/api', routes);
